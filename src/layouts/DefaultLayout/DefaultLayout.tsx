614498247b2522073cca3f9dc75a3c97b0df3dd1
Nathallye Bacelar <nathallyet@gmail.com>
2023-02-20 23:07:00 -0300

    Styles DefaultLayout

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+
+import { LayoutContainer } from "./styles";
+import { Header } from "../../components/Header";
 
 export const DefaultLayout = () => {
   return (
-    <>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/* renderiza o conteúdo dinamicamente */}
-    </>
+    </LayoutContainer>
   );
 };
