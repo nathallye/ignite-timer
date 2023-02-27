b8e43d6e872fdd8f5068072180598c8f1c23cf63
Nathallye Bacelar <nathallyet@gmail.com>
2023-02-27 20:59:17 -0300

    Converting newcycleform to context

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 import { HandPalm, Play } from "phosphor-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
 import { NewCycleForm } from "./components/NewCycleForm";
 import { Countdown } from "./components/Countdown";
@@ -29,10 +32,30 @@ interface CyclesContextType {
 
 export const CyclesContext = createContext({} as CyclesContextType);
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 export const Home = () => {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const { handleSubmit, watch, reset } = newCycleForm; // a função useForm retorna um objeto(que foi armazenada em newCycleForm), e podemos pegar o que iremos usar(e armazenar em constantes) com o object destructuring
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
@@ -52,24 +75,24 @@ export const Home = () => {
     );
   }
 
-  // const createNewCycleHandler = (data: NewCycleFormData) => {
-  //   const id = String(new Date().getTime());
+  const createNewCycleHandler = (data: NewCycleFormData) => {
+    const id = String(new Date().getTime());
 
-  //   const newCycle: Cycle = {
-  //     id: id,
-  //     task: data.task,
-  //     minutesAmount: data.minutesAmount,
-  //     startDate: new Date(),
-  //   };
+    const newCycle: Cycle = {
+      id: id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
 
-  //   // toda vez qua alteramos o estado e esse estado depende da sua versão anterior(antes de alterar),
-  //   // é mais seguro setarmos o valor de estado em formato de função, onde pegamos o estado atual(state), copiamos e por fim adicionamos a nova informação
-  //   setCycles((state) => [...state, newCycle]);
-  //   setActiveCycleId(id); // setamos o id do ciclo atual no estado activeCycleId
-  //   setAmountSecondsPassed(0); // zeramos o contador de quantos segundos já se passaram
+    // toda vez qua alteramos o estado e esse estado depende da sua versão anterior(antes de alterar),
+    // é mais seguro setarmos o valor de estado em formato de função, onde pegamos o estado atual(state), copiamos e por fim adicionamos a nova informação
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id); // setamos o id do ciclo atual no estado activeCycleId
+    setAmountSecondsPassed(0); // zeramos o contador de quantos segundos já se passaram
 
-  //   reset();
-  // };
+    reset();
+  };
 
   const interruptCycleHandler = () => {
     setCycles( // ao interromper um ciclo, será chamada a função que altera o estado dos ciclos(setCycles)
@@ -84,19 +107,26 @@ export const Home = () => {
     setActiveCycleId(null); // por fim, muda o estado da variável que armazena o id do ciclo ativo para null
   };
 
-  // const task = watch("task"); // watch fica observando as alteções em task
-  // const isSubmitDisable = !task; // variável auxiliar para armazer um valor booleano, se task existe(não é null)
+  const task = watch("task"); // watch fica observando as alteções em task
+  const isSubmitDisable = !task; // variável auxiliar para armazer um valor booleano, se task existe(não é null)
 
   return (
     <HomeContainer>
-      <form /*onSubmit={handleSubmit(createNewCycleHandler)}*/>
-        <CyclesContext.Provider value={{
+      <form onSubmit={handleSubmit(createNewCycleHandler)}>
+        <CyclesContext.Provider
+          value={{
             activeCycle,
             activeCycleId,
             markCurrentCycleAsFinished,
             amountSecondsPassed,
-            setAmountSecondsPassedHandler }}>
-          {/* <NewCycleForm /> */}
+            setAmountSecondsPassedHandler
+          }}
+        >
+
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
+
           <Countdown />
         </CyclesContext.Provider>
 
@@ -106,7 +136,7 @@ export const Home = () => {
             Interromper
           </StopCountdownButton>
         ) : (
-          <StartCountdownButton /*disabled={isSubmitDisable}*/ type="submit">
+          <StartCountdownButton disabled={isSubmitDisable} type="submit">
             <Play size={24} />
             Começar
           </StartCountdownButton>
--- src/pages/Home/components/NewCycleForm/NewCycleForm.tsx
+++ src/pages/Home/components/NewCycleForm/NewCycleForm.tsx
@@ -1,27 +1,13 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as zod from "zod";
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+
+import { CyclesContext } from "../../Home";
 
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
 
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, "Informe a tarefa"),
-  minutesAmount: zod
-    .number()
-    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
-    .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
-});
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
-
 export const NewCycleForm = () => {
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: "",
-      minutesAmount: 0,
-    },
-  }); // a função useForm retorna um objeto, e podemos pegar o que iremos usar(e armazenar em constantes) com o object destructuring
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormContainer>
@@ -32,14 +18,15 @@ export const NewCycleForm = () => {
         placeholder="Dê um nome para o seu projeto"
         disabled={!!activeCycle}
         {...register("task")}
-      />
-      {/*O operador spreed pega todas as informações que o register possui e passa para o TaskInput*/}
+      />{/*O operador spreed pega todas as informações que o register possui e passa para o TaskInput*/}
+
       <datalist id="task-suggestions">
         {/* lista de opções para o input*/}
         <option value="Trabalhar" />
         <option value="Assistir aulas de inglês" />
         <option value="Assistir aulas de react" />
       </datalist>
+
       <label htmlFor="minutesAmount">durante</label>
       <MinutesAmountInput
         type="number"
@@ -50,8 +37,8 @@ export const NewCycleForm = () => {
         max={60}
         disabled={!!activeCycle}
         {...register("minutesAmount", { valueAsNumber: true })}
-      />
-      {/*O operador spreed pega todas as informações que o register possui e passa para o MinutesAmountInput*/}
+      />{/*O operador spreed pega todas as informações que o register possui e passa para o MinutesAmountInput*/}
+
       <span>minutos.</span>
     </FormContainer>
   );
