b6bf5c2a2deb99c129e1bdb267c76819bc26cb46
Nathallye Bacelar <nathallyet@gmail.com>
2023-03-04 22:19:03 -0300

    library immer

--- README.md
+++ README.md
@@ -1283,4 +1283,119 @@ export const CyclesContextProvider = ({ children }: CyclesContextProviderProps) 
 
 ### Separando Action Types
 
+
+
 ### Separando Actions
+
+
+
+### Trabalhando com a biblioteca immer
+
+Agora vamos utilizar a biblioteca Immer(https://github.com/immerjs/immer) pra nos ajudar a lidar com os dados da nossa aplicação sem termos que nos preocupar com a imutabilidade do react.
+
+- Para instalar o Immer iremos rodar o comando seguinte:
+
+```
+> npm i immer
+```
+
+- Alterações feitas no arquivo `reducer.ts`:
+
+``` TS
+import { produce } from "immer";
+
+import { ActionTypes } from "./actions";
+
+export interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
+
+interface CyclesState {
+  cycles: Cycle[];
+  activeCycleId: string | null;
+}
+
+export const cyclesReducer = (state: CyclesState, action: any) => {
+  switch (action.type) {
+    case ActionTypes.ADD_NEW_CYCLE:
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id
+      // }
+
+      // depois...
+      return produce(state, (draft) => { // draft é o rascunho, e dentro dele fazemos a alterações que queremos, esse rascunho tem o mesmo formato que o state(CyclesState)
+        draft.cycles.push(action.payload.newCycle); // método push não respeita os conceitos de imutabilidade, mas com a biblioteca immer podemos trabalhar com ele sem nos preocupar
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
+
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      // depois...
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].interruptedDate = new Date();
+      });
+    }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, finishedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      // depois...
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
+      }
+
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].finishedDate = new Date();
+      });
+    }
+
+    default:
+      return state;
+  }
+}
+```
--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,3 +1,5 @@
+import { produce } from "immer";
+
 import { ActionTypes } from "./actions";
 
 export interface Cycle {
@@ -17,37 +19,76 @@ interface CyclesState {
 export const cyclesReducer = (state: CyclesState, action: any) => {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id
+      // }
+
+      // depois...
+      return produce(state, (draft) => { // draft é o rascunho, e dentro dele fazemos a alterações que queremos, esse rascunho tem o mesmo formato que o state(CyclesState)
+        draft.cycles.push(action.payload.newCycle); // método push não respeita os conceitos de imutabilidade, mas com a biblioteca immer podemos trabalhar com ele sem nos preocupar
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
+
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, interruptedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      // depois...
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
       }
 
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].interruptedDate = new Date();
+      });
+    }
+
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      // antes de usar o immer...
+      // return {
+      //   ...state,
+      //   cycles: state.cycles.map((cycle) => {
+      //     if (cycle.id === state.activeCycleId) {
+      //       return { ...cycle, finishedDate: new Date() };
+      //     } else {
+      //       return cycle;
+      //     }
+      //   }),
+      //   activeCycleId: null
+      // }
+
+      // depois...
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId;
+      });
+
+      if (currentCycleIndex < 0) {
+        return state;
       }
 
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, finishedDate: new Date() };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null
-      }
+      return produce(state, (draft) => {
+        draft.activeCycleId = null;
+        draft.cycles[currentCycleIndex].finishedDate = new Date();
+      });
+    }
 
     default:
       return state;
